8cdbc40301b7a2fbb84d8c76ef3c4bcb1f4740c8
Antonio Napolitano <anton@polit.no>
2026-09-17 11:31:43 +0200

    cudann: version string 0.1.1
    The compile-time CUDANN_VERSION_STRING reported in build_info now matches the release.

--- include/cudann/config.hpp
+++ include/cudann/config.hpp
@@ -14,7 +14,7 @@
 #define CUDANN_VERSION_MAJOR 0
 #define CUDANN_VERSION_MINOR 1
 #define CUDANN_VERSION_PATCH 0
-#define CUDANN_VERSION_STRING "0.1.0"
+#define CUDANN_VERSION_STRING "0.1.1"
 
 namespace cudann {
 
